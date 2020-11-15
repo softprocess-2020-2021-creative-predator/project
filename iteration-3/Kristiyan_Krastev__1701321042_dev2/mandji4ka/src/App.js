54f9b367e37760e2e5830fbbb70a495e5fb63539
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-11-15 11:21:20 +0200

    Initial commit

---
+++ iteration-3/Kristiyan_Krastev__1701321042_dev2/mandji4ka/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LoginForm from "./components/login/LoginForm";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm></LoginForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ iteration-3/Kristiyan_Krastev__1701321042_dev2/mandji4ka/src/components/login/LoginForm.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const LoginForm = () => {
+
+  const validateForm = () => {
+    
+  }
+
+  return (
+    <Form>
+      <Form.Group controlId="email">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default LoginForm;
